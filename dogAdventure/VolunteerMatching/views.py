5c0b418cd850b62a02074df9d95fca665d7cee04
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-13 18:08:46 +0900

    FIX DogListFilteringAPI view in VolunteerMatching

--- dogAdventure/VolunteerMatching/views.py
+++ dogAdventure/VolunteerMatching/views.py
@@ -26,12 +26,11 @@ class DogDetailAPI(APIView):
 class DogListFilteringAPI(APIView):
     def get(self, request):
         date = request.GET.get("date", None)
-        region = request.GET.get("region", None)
         transport = request.GET.get("transport", None)
         destination = request.GET.get("destination", None)
 
         date = datetime.datetime.strptime(date + " 00:00:00", '%Y-%m-%d %H:%M:%S')
-        dog = AbandonedDog.objects.filter(Q(datetime=date) & Q(region=region) & Q(transport=transport) & Q(destination=destination))
+        dog = AbandonedDog.objects.filter(Q(datetime=date) & Q(transport=transport) & Q(destination=destination))
         serializer = DogSerializer(dog, many=True)
         return Response(serializer.data)
 
