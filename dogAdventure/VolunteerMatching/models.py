33024be29df2887f0db81dcb65347449bd255a03
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-13 22:45:41 +0900

    FIX AbandonedDog models

--- dogAdventure/VolunteerMatching/models.py
+++ dogAdventure/VolunteerMatching/models.py
@@ -17,7 +17,7 @@ class AbandonedDog(models.Model) :
     region = models.CharField(max_length=64)
     transport = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
-    title = models.TextField()
+    title = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return (self.name + '_' + str(self.id))
