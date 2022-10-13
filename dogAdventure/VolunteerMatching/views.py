cee04e0a3bc9be0cf7762de5ad2a7d5825e65d38
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-13 18:06:56 +0900

    ADD DogListFilteringAPI view in VolunteerMatching

--- dogAdventure/VolunteerMatching/views.py
+++ dogAdventure/VolunteerMatching/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import datetime
+from django.db.models import Q
 
 from .models import AbandonedDog
 from .serializers import DogSerializer
@@ -25,9 +26,13 @@ class DogDetailAPI(APIView):
 class DogListFilteringAPI(APIView):
     def get(self, request):
         date = request.GET.get("date", None)
+        region = request.GET.get("region", None)
+        transport = request.GET.get("transport", None)
+        destination = request.GET.get("destination", None)
+
         date = datetime.datetime.strptime(date + " 00:00:00", '%Y-%m-%d %H:%M:%S')
-        dog = AbandonedDog.objects.get(datetime=date)
-        serializer = DogSerializer(dog)
+        dog = AbandonedDog.objects.filter(Q(datetime=date) & Q(region=region) & Q(transport=transport) & Q(destination=destination))
+        serializer = DogSerializer(dog, many=True)
         return Response(serializer.data)
 
 class ReAPI(APIView):
