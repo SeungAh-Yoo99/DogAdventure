f36d42c5c817d75286e5292db9d89902563fbd6d
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-14 01:05:18 +0900

    FIX DogSerializer in VolunteerMatching

--- dogAdventure/VolunteerMatching/serializers.py
+++ dogAdventure/VolunteerMatching/serializers.py
@@ -17,6 +17,12 @@ class DogSerializer(serializers.ModelSerializer):
     def get_images(self, obj):
         image=obj.image.all()
         return DogImagesSerializer(instance=image, many=True, context=self.context).data
+    def create(self, validated_data):
+        instance = AbandonedDog.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            Image.objects.create(diary=instance, image=image_data)
+        return instance
     class Meta:
         model = AbandonedDog
         fields = ['id', 'name', 'gender', 'datetime', 'title', 'weight', 'info', 'region', 'transport', 'destination', 'images']
