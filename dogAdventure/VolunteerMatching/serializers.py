0387b9f19bdc59703eb6ec25d5658e9fde5b1d41
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-14 00:21:13 +0900

    FIX DogImagesSerializer in VolunteerMatching

--- dogAdventure/VolunteerMatching/serializers.py
+++ dogAdventure/VolunteerMatching/serializers.py
@@ -3,11 +3,15 @@ from rest_framework import serializers
 from .models import AbandonedDog, Image
 
 class DogImagesSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+    # image = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
+    image_url = serializers.SerializerMethodField('get_image_url')
 
     class Meta:
         model = Image
         fields = ['image', ]
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(obj.image.url)
 
 class DogSerializer(serializers.ModelSerializer):
     images = DogImagesSerializer(many=True, read_only=True)
