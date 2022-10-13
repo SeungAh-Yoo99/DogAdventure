5869a283799f461b7bc1cb9504dd97be63696387
SEUNGAH YOO <ysa8497@gmail.com>
2022-10-14 00:51:56 +0900

    FIX

--- dogAdventure/VolunteerMatching/serializers.py
+++ dogAdventure/VolunteerMatching/serializers.py
@@ -13,9 +13,9 @@ class DogImagesSerializer(serializers.ModelSerializer):
 class DogSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
     
-    def get_images(self, obj):
-        image=obj.image_set.all()
-        return DogImagesSerializer(instance=image, many=True, context=self.context).data
+    # def get_images(self, obj):
+    #     image=obj.image_set.all()
+    #     return DogImagesSerializer(instance=image, many=True, context=self.context).data
     class Meta:
         model = AbandonedDog
         fields = ['id', 'name', 'gender', 'datetime', 'title', 'weight', 'info', 'region', 'transport', 'destination', 'images']
